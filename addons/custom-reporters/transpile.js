cc99e408bbfdc30b1f65a8e5343808ac4cba19ab
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2024-02-08 15:11:25 +0000

    Implement vanilla->SA transpilation for procedures_return

--- addons/custom-reporters/transpile.js
+++ addons/custom-reporters/transpile.js
@@ -6,63 +6,63 @@
  * Base transpiler class, to be extended for each transpilation scheme
  */
 class Transpiler {
-    constructor (vm, ScratchBlocks) {
-        this.vm = vm;
-        this.ScratchBlocks = ScratchBlocks;
-        if (this.vm.editingTarget) {
-            this.transpileTargetToSA(this.vm.editingTarget);
-        }
-        const setEditingTarget = this.vm.constructor.prototype.setEditingTarget;
-        const transpilerThis = this;
-        this.vm.constructor.prototype.setEditingTarget = function (targetId) {
-            setEditingTarget.call(this, targetId);
-            transpilerThis.transpileTargetToSA(this.editingTarget);
-        }
-        const toJSON = this.vm.constructor.prototype.toJSON;
-        this.vm.constructor.prototype.toJSON = function (optTargetId) {
-            if (optTargetId) {
-                const target = this.runtime.getTargetById(optTargetId);
-                transpilerThis.transpileTargetToVanilla(target, false);
-                const json = toJSON.call(this, optTargetId);
-                transpilerThis.transpileTargetToSA(target, false);
-                return json;
-            }
-            for (const target of this.runtime.targets) {
-                transpilerThis.transpileTargetToVanilla(target, false);
-            }
-            const json = toJSON.call(this, optTargetId);
-            if (this.editingTarget) {
-                transpilerThis.transpileTargetToSA(this.editingTarget, false);
-            }
-            return json;
-        }
+  constructor(vm, ScratchBlocks) {
+    this.vm = vm;
+    this.ScratchBlocks = ScratchBlocks;
+    if (this.vm.editingTarget) {
+      this.transpileTargetToSA(this.vm.editingTarget);
     }
+    const setEditingTarget = this.vm.constructor.prototype.setEditingTarget;
+    const transpilerThis = this;
+    this.vm.constructor.prototype.setEditingTarget = function (targetId) {
+      setEditingTarget.call(this, targetId);
+      transpilerThis.transpileTargetToSA(this.editingTarget);
+    };
+    const toJSON = this.vm.constructor.prototype.toJSON;
+    this.vm.constructor.prototype.toJSON = function (optTargetId) {
+      if (optTargetId) {
+        const target = this.runtime.getTargetById(optTargetId);
+        transpilerThis.transpileTargetToVanilla(target, false);
+        const json = toJSON.call(this, optTargetId);
+        transpilerThis.transpileTargetToSA(target, false);
+        return json;
+      }
+      for (const target of this.runtime.targets) {
+        transpilerThis.transpileTargetToVanilla(target, false);
+      }
+      const json = toJSON.call(this, optTargetId);
+      if (this.editingTarget) {
+        transpilerThis.transpileTargetToSA(this.editingTarget, false);
+      }
+      return json;
+    };
+  }
 
-    transpileTargetToVanilla(target, shouldEmitWorkspaceUpdate=true) {
-        if (!target.transpiledToSA) return;
-        this._toVanilla(target, shouldEmitWorkspaceUpdate=true);
-        target.transpiledToSA = false;
-    }
+  transpileTargetToVanilla(target, shouldEmitWorkspaceUpdate = true) {
+    if (!target.transpiledToSA) return;
+    this._toVanilla(target, (shouldEmitWorkspaceUpdate = true));
+    target.transpiledToSA = false;
+  }
 
-    transpileTargetToSA(target, shouldEmitWorkspaceUpdate=true) {
-        if (target.transpiledToSA) return;
-        this._toSA(target, shouldEmitWorkspaceUpdate=true);
-        target.transpiledToSA = true;
-    }
+  transpileTargetToSA(target, shouldEmitWorkspaceUpdate = true) {
+    if (target.transpiledToSA) return;
+    this._toSA(target, (shouldEmitWorkspaceUpdate = true));
+    target.transpiledToSA = true;
+  }
 
-    _toVanilla(target, shouldEmitWorkspaceUpdate=true) {
-        throw new Error("transpileTargetToVanilla must be overriden by derived class");
-    }
+  _toVanilla(target, shouldEmitWorkspaceUpdate = true) {
+    throw new Error("transpileTargetToVanilla must be overriden by derived class");
+  }
 
-    _toSA(target, shouldEmitWorkspaceUpdate=true) {
-        throw new Error("transpileTargetToSA must be overriden by derived class");
-    }
+  _toSA(target, shouldEmitWorkspaceUpdate = true) {
+    throw new Error("transpileTargetToSA must be overriden by derived class");
+  }
 }
 
 export class VarTranspiler extends Transpiler {
-    constructor(vm, ScratchBlocks, getWorkspace) {
-        super(vm, ScratchBlocks, getWorkspace);
-    }
+  constructor(vm, ScratchBlocks, getWorkspace) {
+    super(vm, ScratchBlocks, getWorkspace);
+  }
 
     _toVanilla(target, shouldEmitWorkspaceUpdate=true) {
         console.log('tovanilla')
@@ -93,7 +93,44 @@ export class VarTranspiler extends Transpiler {
               }
               case "procedures_return_reporter":
               case "procedures_return_boolean": {
-                break;
+                console.log(block)
+                block.inputs.VALUE = block.inputs.return_value;
+                block.opcode = "data_setvariableto";
+                const proccode = blocks[blocks[target.blocks.getTopLevelScript(blockid)].inputs.custom_block.block].mutation.proccode;
+                const varname = `_return ${proccode}`;
+                let variable = target.lookupVariableByNameAndType(varname);
+                if (!variable) {
+                    variable = {
+                        value: varname,
+                        id: this.ScratchBlocks.utils.genUid(),
+                        variableType: '',
+                    };
+                    target.createVariable(variable.id, variable.value, '');
+                }
+                block.fields.VARIABLE = {
+                    ...variable,
+                };
+                delete block.inputs.return_value;
+                const nextid = this.ScratchBlocks.utils.genUid();
+                blocks[nextid] = {
+                    id: nextid,
+                    opcode: "control_stop",
+                    fields: {
+                        STOP_OPTION: {
+                            value: "this script",
+                        }
+                    },
+                    next: null,
+                    topLevel: false,
+                    parent: blockid,
+                    shadow: false,
+                    mutation: {
+                        tagName: "mutation",
+                        children: [],
+                        hasNext: false,
+                    }
+                }
+                block.next = nextid;
               }
             }
         }
@@ -102,30 +139,30 @@ export class VarTranspiler extends Transpiler {
         }
     }
 
-    _toSA(target, shouldEmitWorkspaceUpdate=true) {
-        console.log('tosa')
-        const blocks = target.blocks._blocks;
-        for (const blockid in blocks) {
-            const block = blocks[blockid];
-            switch (block.opcode) {
-                case "procedures_prototype": {
-                    if (block.mutation.shape === "reporter") {
-                        block.opcode = "procedures_prototype_reporter";
-                    } else if (block.mutation.shape === "boolean") {
-                        block.opcode = "procedures_prototype_boolean";
-                    }
-                    break;
-                }
-                case "procedures_definition": {
-                    if (block.mutation?.shape === "reporter") {
-                        block.opcode = "procedures_definition_reporter";
-                    }
-                    break;
-                }
-            }
+  _toSA(target, shouldEmitWorkspaceUpdate = true) {
+    console.log("tosa");
+    const blocks = target.blocks._blocks;
+    for (const blockid in blocks) {
+      const block = blocks[blockid];
+      switch (block.opcode) {
+        case "procedures_prototype": {
+          if (block.mutation.shape === "reporter") {
+            block.opcode = "procedures_prototype_reporter";
+          } else if (block.mutation.shape === "boolean") {
+            block.opcode = "procedures_prototype_boolean";
+          }
+          break;
         }
-        if (shouldEmitWorkspaceUpdate) {
-            this.vm.emitWorkspaceUpdate();
+        case "procedures_definition": {
+          if (block.mutation?.shape === "reporter") {
+            block.opcode = "procedures_definition_reporter";
+          }
+          break;
         }
+      }
     }
+    if (shouldEmitWorkspaceUpdate) {
+      this.vm.emitWorkspaceUpdate();
+    }
+  }
 }
